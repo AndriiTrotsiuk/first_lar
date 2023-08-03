5ccd67a55a57ebd9765f6f81869e6eca135cf6d3
Andrii Trotsiuk <and1reymail@gmail.com>
2023-08-03 21:28:43 +0300

    view tasks.index

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.default')
+
+@section('content')
+    <h2>All tasks</h2>
+    <form action="/tasks" method="post">
+       {{ csrf_field() }}
+        <label>Task name:
+            <input type="text" name="name">
+        </label>
+        <input type="submit">
+    </form>
+{{--    TODO--}}
+@endsection
